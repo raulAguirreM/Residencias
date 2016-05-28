f344738295216b4afb178d6275fb300c386d9e34
raulAguirreM <raul.aguirremendoza@sistemas.tecsanpedro.edu.mx>
2016-05-27 19:12:22 -0500

    BD Residencias

---
+++ ProyectoIngenieria.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE Proyecto
+
+CREATE TABLE Alumnos(
+Matricula int PRIMARY KEY NOT NULL,
+Nombre VARCHAR(30) NOT NULL,
+ApP VARCHAR (30) NOT NULL,
+ApM VARCHAR (30) NOT NULL,
+Carrera VARCHAR(20) NOT NULL,
+Telefono VARCHAR(10),
+Correo VARCHAR(50),
+NombreProyecto VARCHAR(80),
+Empresa VARCHAR(30))
+
+CREATE TABLE Asesores(
+Id INT IDENTITY (000,1) PRIMARY KEY NOT NULL,
+Nombre VARCHAR(30) NOT NULL,
+ApP VARCHAR (30) NOT NULL,
+ApM VARCHAR(30) NOT NULL,
+Tipo VARCHAR(20) NOT NULL,
+Telefono VARCHAR (10),
+Correo VARCHAR(50),
+Profesion VARCHAR(30))
+
+CREATE TABLE Citas(
+Clave INT IDENTITY (1000,1) PRIMARY KEY,
+Matricula INT NOT NULL,
+Id INT NOT NULL,
+Hora DATETIME NOT NULL)
+
+CREATE TABLE Documentacion(
+Folio INT IDENTITY (2000,1) PRIMARY KEY,
+Solicitud BIT,
+Anteproyecto BIT,
+Carta BIT,
+Dictamen BIT,
+Asignacion BIT,
+AsesorExt BIT,
+Seguimiento BIT,
+Registro Bit,
+CartaAgradecimiento BIT, 
+CDInforme BIT,
+CDRes Bit,
+CartaAcep BIT)
+
+CREATE TABLE REQUISITOS(
+Matricula INT NOT NULL,
+Servicio BIT,
+Creditos BIT,
+Extra BIT)
+
