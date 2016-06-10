c6d972e1ed867ffd6c50a848ff4bdb1397ce5cc4
raulAguirreM <raul.aguirremendoza@sistemas.tecsanpedro.edu.mx>
2016-06-09 19:13:40 -0500

    Nada

--- ProyectoIngenieria/ProyectoIngenieria/alumnos.Designer.cs
+++ ProyectoIngenieria/ProyectoIngenieria/alumnos.Designer.cs
@@ -206,6 +206,7 @@
             this.button1.TabIndex = 18;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // alumnos
             // 
